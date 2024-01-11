264a365d821d4f34a3cb65ea9ded13ada8a6be35
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-11 16:54:02 +0200

    init

--- TowDifferentBorderAnimationInSwiftUI/ContentView.swift
+++ TowDifferentBorderAnimationInSwiftUI/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var startR: Double = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 400)
+                .overlay {
+                    AngularGradient.init(gradient: Gradient(colors: [.clear, .clear, .clear, .clear, .blue]), center: .center, angle: .degrees(300))
+                        .frame(width: 490, height: 490)
+                        .rotationEffect(.degrees(startR))
+                        .mask {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(lineWidth: 6)
+                                .frame(width: 297, height: 395)
+                        }
+                }
         }
-        .padding()
+        .onAppear() {
+            withAnimation(.linear(duration: 5).repeatForever(autoreverses: false)) {
+                startR = 360
+            }
+        }
     }
 }
 
