3876bae4de5d2fac22ed38a4bb223afaa03f6b97
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-11 16:30:56 +0200

    Initial Commit

---
+++ TowDifferentBorderAnimationInSwiftUI/TowDifferentBorderAnimationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowDifferentBorderAnimationInSwiftUIApp.swift
+//  TowDifferentBorderAnimationInSwiftUI
+//
+//  Created by test on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TowDifferentBorderAnimationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
